390e1fcdcc719a39ce9d1e42c539a78f16e55286
talonmyburgh <myburgh.talon@gmail.com>
2025-03-24 21:58:47 +0200

    ** use single quotes in string

--- DynSpecMS/ClassSaveResults.py
+++ DynSpecMS/ClassSaveResults.py
@@ -64,10 +64,10 @@ class ClassSaveResults(object):
         self.CatFlux=self.CatFlux.view(np.recarray)
         
         os.system("rm -rf %s"%self.DIRNAME)
-        os.system(f"mkdir -p {os.path.join(self.DIRNAME,"TARGET")}")
-        os.system(f"mkdir -p {os.path.join(self.DIRNAME, "OFF")}")
-        os.system(f"mkdir -p {os.path.join(self.DIRNAME,"TARGET_W")}")
-        os.system(f"mkdir -p {os.path.join(self.DIRNAME,"OFF_W")}")
+        os.system(f"mkdir -p {os.path.join(self.DIRNAME,'TARGET')}")
+        os.system(f"mkdir -p {os.path.join(self.DIRNAME,'OFF')}")
+        os.system(f"mkdir -p {os.path.join(self.DIRNAME,'TARGET_W')}")
+        os.system(f"mkdir -p {os.path.join(self.DIRNAME,'OFF_W')}")
         #os.system("mkdir -p %s/PNG"%self.DIRNAME)
 
     def tarDirectory(self):
